c26763a735db4a006c982d07e895d561cf9516c2
Matt Pahuta <mhpahuta@gmail.com>
2024-02-16 09:34:26 -0800

    Update tw config, add google fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
+
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
 			fontFamily: {
 				'sans': ['Inter', ...defaultTheme.fontFamily.sans],
-				'mono': ['Source Code Pro', ...defaultTheme.fontFamily.mono]
+				'mono': ['"Source Code Pro"', ...defaultTheme.fontFamily.mono]
 			}
 		},
-	},
-	plugins: [],
-}
+  },
+  plugins: [],
+};
